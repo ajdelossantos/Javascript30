0287059b695d3bc686fc94c5b033513fc496c2dd
Alvin James Delos Santos <a.jamesdelossantos@gmail.com>
2018-02-12 19:38:17 -0800

    11 - Add skip function and event listeners

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -4,7 +4,7 @@ const video = document.querySelector('.viewer');
 const progress = document.querySelector('.progress');
 const progressBar = document.querySelector('.progress__filled');
 const toggle = document.querySelector('.toggle');
-const skipButton = document.querySelectorAll('[data-skip]');
+const skipButtons = document.querySelectorAll('[data-skip]');
 const ranges = document.querySelectorAll('.player__slider');
 
 // Build functions
@@ -16,6 +16,22 @@ function togglePlay() {
   };
 };
 
+function updateButton() {
+  // this is bound to the video
+  const icon = this.paused ? '►' : '❚ ❚';
+  console.log(icon);
+  toggle.textContent = icon;
+};
+
+function skip() {
+  console.log(this.dataset.skip);
+  video.currentTime += parseFloat(this.dataset.skip);
+};
+
 // hookup event listeners
 video.addEventListener('click', togglePlay);
+video.addEventListener('play', updateButton);
+video.addEventListener('pause', updateButton);
+
 toggle.addEventListener('click', togglePlay);
+skipButtons.forEach(button => button.addEventListener('click', skip));
